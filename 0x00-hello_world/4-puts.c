4cf901d87946668d5ecb59cad8b4195a9cdad818
Mthembiseni <ngemamthembiseni@gmail.com>
2023-02-19 05:08:50 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#Include < stdio.h >
+/**
+ * main - a that prints a line with puts function
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts ("\"Programming is like building a multilingual puzzle");
+Return (0);
+}
